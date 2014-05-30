9a8984acc83bf963f1ad9792a75e18c0892e68ba
Jonathan Hartman <j@p4nt5.com>
2014-05-29 21:36:20 -0700

    Get API test passing

--- test/integration/default/serverspec/localhost/ports_spec.rb
+++ test/integration/default/serverspec/localhost/ports_spec.rb
@@ -15,6 +15,21 @@ describe 'Longshoreman-related ports' do
 
   it 'is returning valid JSON from the Docker API over port 443' do
     uri = URI('https://localhost/containers/json')
-    expect(JSON.parse(Net::HTTP.get(uri)).class).to eq(Array)
+    opt = {
+      use_ssl: true,
+      # TODO: Shouldn't VERIFY_PEER pass too?
+      verify_mode: OpenSSL::SSL::VERIFY_NONE,
+      ca_file: '/opt/longshoreman/tls/ca.pem',
+      cert: OpenSSL::X509::Certificate.new(
+        File.open('/opt/longshoreman/tls/cert.pem').read
+      ),
+      key: OpenSSL::PKey::RSA.new(
+        File.open('/opt/longshoreman/tls/key.pem').read
+      )
+    }
+    response = Net::HTTP.start(uri.host, uri.port, opt) do |http|
+      http.request(Net::HTTP::Get.new(uri.path))
+    end
+    expect(JSON.parse(response.body).class).to eq(Array)
   end
 end
